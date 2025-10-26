9b02421ddff512b1eee12f1fc2ee156eeb264f69
unknown <swarnendubanerjee123@gmail.com>
2025-10-26 11:50:13 +0530

    jenkins,sonarqube done aws deployment left

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup,find_packages
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
-
+#jenkins,sonarqube done
+# aws deployment left
 setup(
     name="AT_YOUR_SERVICE",
     version="0.1",
